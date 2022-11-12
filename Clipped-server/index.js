2965f48491ef3a51db0f76499eb06a80625f535a
izarooni <coalasonly@gmail.com>
2022-11-12 03:49:39 -0700

    ocd code clean up

--- Clipped-server/index.js
+++ Clipped-server/index.js
@@ -44,23 +44,23 @@ import RegisterPage from './src/pages/register.js'
 import * as ProfileHandler from './src/pages/profile.js'
 import * as VideoHandler from './src/pages/video.js'
 
-app.post('/navbar', (req, res) => Navbar(req, res));
-app.post('/login', (req, res) => LoginPage(req, res));
-app.post('/register', (req, res) => RegisterPage(req, res));
+app.post('/navbar',   /**/(req, res) => Navbar(req, res));
+app.post('/login',    /**/(req, res) => LoginPage(req, res));
+app.post('/register', /**/(req, res) => RegisterPage(req, res));
 
-app.get /**/('/video/preview/:video', (req, res) => VideoHandler.VideoPreview(req, res));
-app.get /**/('/video/:ID', (req, res) => VideoHandler.VideoStream(req, res));
-app.post/**/('/video/upload', upload.single('file'), (req, res) => VideoHandler.VideoUpload(req, res));
-app.post/**/('/video/details', (req, res) => VideoHandler.VideoUpdate(req, res));
-app.get /**/('/video/details/:ID', (req, res) => VideoHandler.VideoDetails(req, res));
-app.post/**/('/video/comment/:ID', (req, res) => VideoHandler.VideoComment(req, res));
-app.post/**/('/video/comments/:ID/:thread?', (req, res) => VideoHandler.VideoComments(req, res));
+app.get /**/('/video/:ID',                          /**/(req, res) => VideoHandler.VideoStream(req, res));
+app.post/**/('/video/upload', upload.single('file'),/**/(req, res) => VideoHandler.VideoUpload(req, res));
+app.post/**/('/video/details',                      /**/(req, res) => VideoHandler.VideoUpdate(req, res));
+app.get /**/('/video/preview/:ID',                  /**/(req, res) => VideoHandler.VideoPreview(req, res));
+app.get /**/('/video/details/:ID',                  /**/(req, res) => VideoHandler.VideoDetails(req, res));
+app.post/**/('/video/comment/:ID',                  /**/(req, res) => VideoHandler.VideoComment(req, res));
+app.post/**/('/video/comments/:ID/:thread?',        /**/(req, res) => VideoHandler.VideoComments(req, res));
 
 app.post/**/('/videos/:a/:b/:c?', (req, res) => VideoHandler.VideoSearch(req, res));
 
-app.get /**/('/profile/avatar/:ID', (req, res) => ProfileHandler.ProfileAvatarPage(req, res));
-app.post/**/('/profile/update', (req, res) => ProfileHandler.ProfileUpdate(req, res));
-app.post/**/('/profile/:ID', (req, res) => ProfileHandler.ProfilePage(req, res));
+app.get /**/('/profile/avatar/:ID', /**/(req, res) => ProfileHandler.ProfileAvatarPage(req, res));
+app.post/**/('/profile/update',     /**/(req, res) => ProfileHandler.ProfileUpdate(req, res));
+app.post/**/('/profile/:ID',        /**/(req, res) => ProfileHandler.ProfilePage(req, res));
 
 app.listen(8888, () => console.log(`
 ONLIN 
