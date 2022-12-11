af196c6f1b2dd3b8866e40db8b4077ac6d609018
izarooni <coalasonly@gmail.com>
2022-12-11 07:05:16 -0700

    delete localStorage.friends on logout
    - clears the navbar friends menu

--- Clipped-website/pages/logout.js
+++ Clipped-website/pages/logout.js
@@ -3,6 +3,7 @@ import { useEffect } from 'react';
 export default function Logout() {
     useEffect(() => {
         document.cookie = 'user=;Max-Age=0'
+        delete localStorage.friends;
 
         window.location = '/'
     }, []);
